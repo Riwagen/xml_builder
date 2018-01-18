1af3cb88d4a27d137f22680dfad59c21169f34c4
Misha <forewor@gmail.com>
2018-01-18 10:53:13 +0200

    Update indented.ex

--- lib/xml_builder/format/indented.ex
+++ lib/xml_builder/format/indented.ex
@@ -1,5 +1,6 @@
 defmodule XmlBuilder.Format.Indented do
   def indentation(level, options) do
+    IO.inspect option
     whitespace = Keyword.get(options, :whitespace, "")
 
     String.duplicate(whitespace, level)
